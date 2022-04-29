0f7d7d878c9b614042c7b124499f3c4b5e047b4b
Alex Free <41807606+HippomanX@users.noreply.github.com>
2022-04-29 15:24:39 -0700

    afterclass1
    added bot class, changed structure of main class, did column method

---
+++ Connect4.java
@@ -0,0 +1,93 @@
+public class Connect4 extends Bot{ // things to fix: spacing, method contracts, making sure we standardize thinking of top vs. bottom
+   //DECIDE WHETHER OR NOT WE DO 2 CLASSES, WHETHER TO EXTEND BOT?
+   
+   //instance variables
+   private int player;
+   private int width;
+   private int height;
+   private int board[][];
+   
+   //constructor
+   public Connect4(int w, int h, String p){
+      if(p.equals("red"))
+         this.player = 1;
+      if(p.equals("yellow"))
+         this.player = 2;
+      else{
+         StdOut.println("Invalid color, player set to red");
+         this.player = 1;
+      }
+      
+      this.width = 7; //ignore user instructions for now
+      this.height = 6;
+      
+      this.board = new int[w][h];
+      for(int x = 0; x < this.width; x++){
+         for(int y = 0; y < this.height; y++){
+            this.board[x][y] = 0; //0 represents empty, 1 represents red, 2 represents yellow
+         }
+      }
+   }
+   
+   //
+   public boolean drop(int column, int user){
+      int rowToPlace = 0;
+      //find first empty space thinking from the bottom
+      for(int i = 0; i < height; i++){
+         if(board[column][i] == 0){
+            board[column][i] = user;
+            return true;
+         }
+         else return false; //if not, do nothing - return false so we know it didn't work
+      }
+      return false; //just in case, this line should never happen (ex. height = 0)
+      
+      
+   }
+   
+   
+   public void print(){
+      //toString, 0=empty=space, 1=red=R, 2=yellow=Y
+      
+      for (int i = 0; i < this.board.length; i++){
+         for(int j = 0; j < this.board[i].length; j++){
+            char temp = ' ';
+            
+            if (this.board[i][j] == 1) temp = 'R';
+            if (this.board[i][j] == 2) temp = 'Y';
+            
+            StdOut.print(" [" + temp + "]");
+            
+         }
+         StdOut.println();
+      }
+   }
+   
+   public int gameStatus(){
+      //returns int of player that won, 0 for draw
+      
+      //for loop if 4 are in a row
+         //for loop for vertical and horizontal terminates -> i < height - 4, width - 4
+         //diagonal will be annoying, look at board and think of possible options
+         
+         
+      //for loop for if the board is full
+      
+      /*for( EACH ROW ){
+         if ALL COLUMNS ARE FULL return 0;
+      }*/
+      return 0; //placeholder
+   }
+   public boolean columnIsFull(int column){
+      
+      //if the first row of the column is full, return true
+      if(board[column][this.height] != 0) return true;
+      else return false;
+      
+   }
+   
+   public void main(String[] args){
+      //"ui", std in, std out, print after every turn, check win vs. draw, etc
+   }
+   
+}
