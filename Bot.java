bfad96246d2663dd3baea94d769dca05888d670c
Alex Free <41807606+HippomanX@users.noreply.github.com>
2022-05-09 15:28:22 -0700

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -1,14 +1,16 @@
 public class Bot{
    //bot stuff
    private int player;
+   //private Connect4 connect;
+   private double min;
+   private double max;
+   
    public Bot(int p){
       this.player = p;
    }
    
-   public int score(Connect4 c){
+   public int minimax(Connect4 c/*, int depth, int alpha, int beta*/){
       //recursive algorithm that calculates score for each move
-      //
-      
       if (c.gameStatus() == this.player){
          return 1;
       }
@@ -22,21 +24,21 @@ public class Bot{
       for(int i = 0; i < c.openMoves().length; i++){
          c.drop(c.openMoves()[i], this.player);
          //MAY NEED METHOD TO "UNDROP"
-         total += this.score(c);
+         total += this.minimax(c);
          c.undrop(c.openMoves()[i], this.player);
       }
       return total;
    }
-   public int minimax(Connect4 c){
+   public int findMove(Connect4 c){
       //returns best move as column integer
       //return 0;
       int bestMove = 0;
       double bestTotal = -Double.POSITIVE_INFINITY;
       for(int i = 0; i < c.openMoves().length; i++){
          c.drop(c.openMoves()[i], this.player);
-         if(this.score(c) > bestTotal){
+         if(this.minimax(c) > bestTotal){
             bestMove = i;
-            bestTotal = this.score(c);
+            bestTotal = this.minimax(c);
          }
          c.undrop(c.openMoves()[i], this.player);
       }
